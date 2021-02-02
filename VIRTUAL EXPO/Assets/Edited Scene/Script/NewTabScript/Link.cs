4ce7e05ec0b892af3e41947818a259aa70558383
Syamsul Nizar <syamsulnizar@telkomuniversity.ac.id>
2021-02-02 16:24:54 +0700

    New Tab Script

---
+++ VIRTUAL EXPO/Assets/Edited Scene/Script/NewTabScript/Link.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://www.youtube.com/watch?v=qqOqLNqAdDo&t=5s");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
---
+++ VIRTUAL EXPO/Assets/Edited Scene/Script/NewTabScript/download.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class download : MonoBehaviour
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?id=1R-k7rbV6vm_56MeULAt0zs6yFB69sIIL&export=download");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
