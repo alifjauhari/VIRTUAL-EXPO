f88fa59e7fb12a7aa3a689d81df0fd152d7ceeb8
Syamsul Nizar <syamsulnizar@telkomuniversity.ac.id>
2021-02-03 01:14:07 +0700

    Link YT & Fullscreen Brosur (ITB)

---
+++ VIRTUAL EXPO/Assets/Edited Scene/Script/NewTabScript/linkITB.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class linkITB : MonoBehaviour 
+{
+
+	public void OpenLinkJSPlugin()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://www.youtube.com/watch?v=V4B82wgFc_U");
+		#endif
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
