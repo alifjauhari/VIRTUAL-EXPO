26ef1c9801950b78ceac0ad16858766ea29a46b3
Syamsul <syamsulnizar.sn@gmail.com>
2021-06-04 13:50:58 +0700

    UI Change Scene

---
+++ VIRTUAL EXPO/Assets/Edited Scene/Script/Trigger/zoomdoor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class zoomdoor : MonoBehaviour
+{
+
+    public GameObject trigger;
+ 
+ void Start () {
+         trigger.SetActive (false);
+     }
+ 
+ void OnTriggerEnter(Collider _col){
+         if (_col.gameObject.CompareTag ("door")) {
+             trigger.SetActive (true);
+                 }
+         }
+
+         void OnTriggerExit(Collider _col){
+         if (_col.gameObject.CompareTag ("door")) {
+             trigger.SetActive (false);
+         }
+     }
+}
