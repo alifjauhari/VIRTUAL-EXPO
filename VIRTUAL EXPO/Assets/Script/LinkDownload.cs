b3f391e25d580fde51ea23fc85bc50dc1a32d302
Alif Kamal Jauhari <alifkamal@student.telkomuniversity.ac.id>
2021-06-09 17:44:47 +0700

    Revisi Desain MainMenu, template booth,  scriptdownload

---
+++ VIRTUAL EXPO/Assets/Script/LinkDownload.cs
@@ -0,0 +1,264 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class LinkDownload : MonoBehaviour
+{
+
+    public void Download1()
+	{
+		#if !UNITY_EDITOR
+		openWindow(" ");
+		#endif
+	}
+
+       public void Download2()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1cWS3Brx9PwnI8IXGT4bcdhQeNgj2-p_i");
+		#endif
+	}
+
+       public void Download3()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1VDr73mtrXA_0E96pCw8sIQevSRclfZVR");
+		#endif
+	}
+
+       public void Download4()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1Q-hxlNW21CwMcKicqx0Jzx8E2qWY6pEJ");
+		#endif
+	}
+
+       public void Download5()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1GSZuW57GMQuQTqYKZ1LBgczAtMi1cdmt");
+		#endif
+	}
+
+       public void Download6()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=14mqxBSI7w1oH5aEbfm9rHNpSZZ5yQRtA");
+		#endif
+	}
+
+       public void Download7()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1rAM5-NNkEBHxS8kzqxbSMU9bDVY8jzIg");
+		#endif
+	}
+
+       public void Download8()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1rAM5-NNkEBHxS8kzqxbSMU9bDVY8jzIg");
+		#endif
+	}
+
+       public void Download9()
+	{
+		#if !UNITY_EDITOR
+		openWindow(" ");
+		#endif
+	}
+
+       public void Download10()
+	{
+		#if !UNITY_EDITOR
+		openWindow(" ");
+		#endif
+	}
+
+       public void Download11()
+	{
+		#if !UNITY_EDITOR
+		openWindow(" ");
+		#endif
+	}
+
+       public void Download12()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1K8rYmZfH2Ch3LzZwWmVqaqWaFRGmthr6");
+		#endif
+	}
+
+       public void Download13()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1Hju598bukJ3aVYGp8oD86rPnoTfqh19d");
+		#endif
+	}
+
+       public void Download14()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=15Xuh3sLn7gInNyBNz9a275iNhKbwcPQk");
+		#endif
+	}
+
+       public void Download15()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=17PXCaaWP_Coi0b91lKVASw52hZNDpsHO");
+		#endif
+	}
+
+       public void Download16()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1jUDwitHiKzn_nr45_dQMZ0UddydTasW9");
+		#endif
+	}
+
+       public void Download17()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1lX_th0dCkA3FYVJEwdat1N6ZZqpawIlb");
+		#endif
+	}
+
+       public void Download18()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1-xnfDp_XwJJtRGilu1Kxfilnyd9W1Ckk");
+		#endif
+	}
+
+       public void Download19()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1XjGmxP05qdmPwAqMKQyYMQMFx2UvQg59");
+		#endif
+	}
+
+       public void Download20()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1jbCMGqNyNK4LNOhJwzCLmVoGqg8sU5i2");
+		#endif
+	}
+
+       public void Download21()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1_N0ODGO0nfNkjyEVdAjTmFpGHQqm808o");
+		#endif
+	}
+
+       public void Download22()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1PcWe_MRa70gFiCjpZEdrm5EFUPYUzfDD");
+		#endif
+	}
+
+       public void Download23()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1duq7AsyOp5XHiDvNBW0LKV1TMhsLU39w");
+		#endif
+	}
+
+       public void Download24()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1JvOCIAVPOAz0pF5oLrPkK9jWpb0urRAb");
+		#endif
+	}
+
+       public void Download25()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1dDl2x8y6Q7JL0IpzyA9Lz3gQlrQHRiii");
+		#endif
+	}
+
+       public void Download26()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1xBmJBeg5LYi1mZGFQPFxJOwkRF4362-V");
+		#endif
+	}
+
+       public void Download27()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1RTaPK4ENMy_a8oqP4fWcrun-jIwJs01N");
+		#endif
+	}
+
+       public void Download28()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1uF1ha1muPrLwmHv7l4PdvvtlukTR4FT5");
+		#endif
+	}
+
+       public void Download29()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1IUqbprVP4elpCjUfEp9lmAMv_U2iRkax");
+		#endif
+	}
+
+       public void Download30()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1bnFXS7ahYpYV1pZWz1aQ37hOt4ec2TCc");
+		#endif
+	}
+
+       public void Download31()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=19nETuIE1qwt9uxOMRBaAvg2bzght2IH9");
+		#endif
+	}
+
+       public void Download32()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1UcA81y6MJXFGKJUyIv2fzFanlZq5f7_k");
+		#endif
+	}
+
+       public void Download33()
+	{
+		#if !UNITY_EDITOR
+		openWindow(" ");
+		#endif
+	}
+
+      public void Download34()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1teC3cm4dugh9eu6TFb3ILYGEW-pyF6pX");
+		#endif
+	}
+
+      public void Download35()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1bzeisvgoNyjMNwiOnvrjQJx_T54ZBr-2");
+		#endif
+	}
+
+      public void Download36()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://drive.google.com/uc?export=download&id=1rAM5-NNkEBHxS8kzqxbSMU9bDVY8jzIg");
+		#endif
+	}
+
+    
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+}
