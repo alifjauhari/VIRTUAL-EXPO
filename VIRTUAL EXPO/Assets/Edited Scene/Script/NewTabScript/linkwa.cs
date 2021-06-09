3cd9b3342cbe7c4c756dc4ee7f7bc1b4ed12959c
Syamsul <syamsulnizar.sn@gmail.com>
2021-06-09 15:25:24 +0700

    Benerin UI

--- VIRTUAL EXPO/Assets/Edited Scene/Script/NewTabScript/linkwa.cs
+++ VIRTUAL EXPO/Assets/Edited Scene/Script/NewTabScript/linkwa.cs
@@ -5,10 +5,10 @@ using UnityEngine.UI;
 public class linkwa : MonoBehaviour 
 {
 
-	public void OpenLinkWhatsApp()
+	public void AzamZamhuri32()
 	{
 		#if !UNITY_EDITOR
-		openWindow("https://chat.whatsapp.com/IvPmD4eumIv9yqAKTf2ebC");
+		openWindow("https://wa.me/6287730000207");
 		#endif
 	}
 
