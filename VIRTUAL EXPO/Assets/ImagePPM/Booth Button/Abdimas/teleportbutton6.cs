79e0b646528e5342be8d24708e8df590434c6d0f
Syamsul <syamsulnizar.sn@gmail.com>
2021-08-27 08:02:14 +0700

    Teleportation

---
+++ VIRTUAL EXPO/Assets/ImagePPM/Booth Button/Abdimas/teleportbutton6.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleportbutton6 : MonoBehaviour
+{
+
+    public GameObject trigger;
+ 
+ void Start () {
+         trigger.SetActive (false);
+     }
+ 
+ void OnTriggerEnter(Collider _col){
+         if (_col.gameObject.CompareTag ("buttontele6")) {
+             trigger.SetActive (true);
+                 }
+         }
+
+         void OnTriggerExit(Collider _col){
+         if (_col.gameObject.CompareTag ("buttontele6")) {
+             trigger.SetActive (false);
+         }
+     }
+}
